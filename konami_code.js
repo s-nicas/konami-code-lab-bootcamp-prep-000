b88b8bfd169ea37f80fbc15e864eecbc68a89bed
Sonia  D <sduarte22@gmail.com>
2018-03-15 03:04:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+var body = document.querySelector('body');
+  body.addEventListener('keydown',function(e){
+  
+    var key = parseInt(e.detail || e.which);
+    var index = 0; 
+    if (key===code[index]){
+      index ++;
+      
+      if (index===code.length){
+        window.alert ("YAY!"); 
+        
+        index =0;
+      }
+    }
+    
+    else {
+      index =0;
+    }
+   
+    
+  });
 }
+
+
+
